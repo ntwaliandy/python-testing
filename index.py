69950ff035094390af3001e65e96100826557e8c
ntwaliandy <ntwaliandy90@gmail.com>
2021-07-29 20:20:02 +0300

    final

--- index.py
+++ index.py
@@ -2,53 +2,39 @@
 
 
 class Person:
-	"""
-	Declaring a function
-	"""
+	
 	def __init__(self, name, age):
+		"""
+	inputs:
+		-user name
+		-user age
+	outputs
+		-null
+	"""
 		self.name = name
 		self.age = age
 
-		"""
-		displaying user inputs in a preferable form.
-		"""
+		
 	def myFunc(self):
+		"""
+		inputs:
+			-null
+		ouputs:
+			-user name
+			-user age
+		"""
 		print("Hello " + " " + self.name)
 		print("You are " + " " + str(self.age) + " " + "old!")
 		print("Welcome to the family!");
 
 
-"""
-taking user input names and Age
-"""
 names = input("Enter Your name:  ")
 Age = int(input("Enter Your age: "))
 
-
 """
-assigning user inputs in the class
+names = names,
+Age = age
 """
 final = Person(names, Age)
-
-"""
-Testing the function if it's true then the results should be printed!
-"""
-
-def testFunc():
-		assert Person(
-			names == names,
-			Age == Age,
-
-		)
-		
-"""
-calling the function testFunc()
-"""
-testFunc()
-
-
-"""
-calling the function myFunc()
-"""
 final.myFunc()
 
---
+++ test.py
@@ -0,0 +1,8 @@
+from index import Age, names, final
+
+class Testing:
+    def testFunc():
+        assert Age == final.age
+        assert names == final.name
+
+    testFunc()
